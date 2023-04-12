80a33081146321f405250fb07a443e6a7627ad78
ferlui32 <luizferri@gmail.com>
2023-04-12 20:20:55 +0100

    added users

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require("express");
 
+require("dotenv").config();
+
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 6000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favorite movie list");
 };
 
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const usersHandler = require("./usersHandler")
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", usersHandler.getUser);
+app.get("/api/users/:id", usersHandler.getUserById)
+
+const database = require("./database");
 
 app.listen(port, (err) => {
   if (err) {
